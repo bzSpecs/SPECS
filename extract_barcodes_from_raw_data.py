1e573ba89e232d57a867f9062a3289dfff138a17
Bar Zaruk <barz.specs@gmail.com>
2020-08-09 22:57:50 +0300

    fix

--- extract_barcodes_from_raw_data.py
+++ extract_barcodes_from_raw_data.py
@@ -10,8 +10,10 @@ output_csv_path = sys.argv[3]
 top_tresh_3_barcodes_df = pd.read_csv(top_tresh_3_barcodes_csv_path)
 raw_data_df = pd.read_csv(raw_data_csv)
 
-merged = top_tresh_3_barcodes_df.set_index('barcode').join(
-    raw_data_df.set_index('barcode')).reset_index()
+merged = raw_data_df.set_index('barcode').join(
+    top_tresh_3_barcodes_df.set_index('barcode')).reset_index()
+merged.dropna(axis=0, subset=['promoter'], inplace=True)
+merged.drop(columns=['promoter'], inplace=True)
 
 # create the folder if not exists yet
 if not os.path.exists(os.path.dirname(output_csv_path)):
