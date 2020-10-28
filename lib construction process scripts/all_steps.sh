8807b3940e22c7f5d3cf7ade935442c5f0bbd684
Blosberg <bosberg@dal.ca>
2020-10-28 13:54:53 +0100

    Add comments to all_steps processing lines

--- lib construction process scripts/all_steps.sh
+++ lib construction process scripts/all_steps.sh
@@ -9,12 +9,16 @@ sample_name=$3 # proper name this sample will be called
 mkdir -p library_preparation
 mkdir -p logs
 
-#Fastqc pre
+#Fastqc pre ==> Just quality control, and done only to establish a baseline
+# so that when we run QC again after trimming, we can compare and see the
+# effects of trimming.
 echo "Running FastQC pre-clipping"
 mkdir -p library_preparation/Fastqc_pre
 mkdir -p logs/Fastqc_pre/
 mkdir -p library_preparation/Fastqc_pre/$sample_name
 fastqc -o library_preparation/Fastqc_pre/$sample_name -t 4 --extract $read2 1> logs/Fastqc_pre/$sample_name.log 2>&1
+#         ^ output_dir                               ^ -t = num threads;
+#                                                          ^ --extract=don't compress (zip) output                 ^ log output
 
 
 # files:
@@ -22,15 +26,18 @@ fastqc -o library_preparation/Fastqc_pre/$sample_name -t 4 --extract $read2 1> l
 # unclipped_$sample_name - the reads that failed to clip over the parameters
 # pre-clipped: GATATCGCGGCCGCCCTGCAGGCGTTGCTAGGCAACCATCCGTTGCTAGGCAACCGCTCGTTGCTAGGCAACCTGACGTTGCTAGGCAACCCAGCGTTGCTAGGCAACCATGCGTTGCTAGGCAACCCGTCGTTGCTAGGCAACCGGCGC
 # post-clipped: CGTTGCTAGGCAACCATCCGTTGCTAGGCAACCGCTCGTTGCTAGGCAACCTGACGTTGCTAGGCAACCCAGCGTTGCTAGGCAACCATGCGTTGCTAGGCAACCCGTCGTTGCTAGGCAACC
+# N.B. repeat:  CGTTGCTAGGCAACC   CGTTGCTAGGCAACC   CGTTGCTAGGCAACC   CGTTGCTAGGCAACC   CGTTGCTAGGCAACC   CGTTGCTAGGCAACC   CGTTGCTAGGCAACC
+# + linkers:                   ^^^               ^^^               ^^^...
+
 #clipping
 echo "Clipping"
 mkdir -p logs/clip
 mkdir -p library_preparation/fastq_clip
-cutadapt -g CGGCCGCCCTGCAGG \
-         -a GGCGCGCCAGACGCT \
-         -O 3 -n 2 \
+cutadapt -g CGGCCGCCCTGCAGG \ # Define the adapter ligated to the 5' end. The adapter and any preceding bases are trimmed
+         -a GGCGCGCCAGACGCT \ # Define the adapter ligated to the 3' end. The adapter and subsequent bases are trimmed
+         -O 3 -n 2 \  # minimum overlap between read and adapter for an adapter to be found
          -o library_preparation/fastq_clip/clipped_$sample_name.fastq -m 30 \
-         --untrimmed-output library_preparation/fastq_clip/unclipped_$sample_name.fastq \
+         --untrimmed-output library_preparation/fastq_clip/unclipped_$sample_name.fastq \ # output reads that do not contain any adapter here
          $read2 \
          1>>logs/clip/$sample_name.log 2>&1
 
@@ -40,6 +47,8 @@ mkdir -p logs/Fastqc_post
 mkdir -p library_preparation/Fastqc_post
 mkdir -p library_preparation/Fastqc_post/clipped_$sample_name
 fastqc -o library_preparation/Fastqc_post/clipped_$sample_name -t 4 --extract library_preparation/fastq_clip/clipped_$sample_name.fastq  1> logs/Fastqc_post/$sample_name.log 2>&1
+#         ^ output_dir                                          ^ -t = num threads;  ^ --extract=don't compress (zip) output                 ^ log output
+
 
 #align
 echo "Aligning"
@@ -47,17 +56,17 @@ mkdir -p library_preparation/bowtie
 mkdir -p library_preparation/bowtie/$sample_name
 mkdir -p logs/bowtie
 mkdir -p logs/samtools
-bowtie2 --very-sensitive --norc \
-        --met-file library_preparation/bowtie/$sample_name/met_$sample_name.txt -p 4\
-        -x bowtie2_lib_D7/D7 \
-        -U library_preparation/fastq_clip/clipped_$sample_name.fastq \
-        -S library_preparation/bowtie/$sample_name/$sample_name.sam &> logs/bowtie/$sample_name.log
+bowtie2 --very-sensitive --norc \  # sets args. See: bowtie2 --help | grep very-sensitive
+        --met-file library_preparation/bowtie/$sample_name/met_$sample_name.txt -p 4\  # output metrics.
+        -x bowtie2_lib_D7/D7 \ # N.B. Need to see this reference.
+        -U library_preparation/fastq_clip/clipped_$sample_name.fastq \  # -U : unpaired list of files to be aligned --N.B.: why unpaired? depends on reference.
+        -S library_preparation/bowtie/$sample_name/$sample_name.sam &> logs/bowtie/$sample_name.log # Define output files & logs
 samtools view -F 4 library_preparation/bowtie/$sample_name/$sample_name.sam | \
-         cut -f1,3 | \
-         sort -k1 > library_preparation/bowtie/$sample_name/mapped_$sample_name.txt 2>> logs/samtools/$sample_name.log
-samtools view -f 4 library_preparation/bowtie/$sample_name/$sample_name.sam | \
-         cut -f1,10 | \
-         sort -k1> library_preparation/bowtie/$sample_name/unmapped_$sample_name.txt 2>> logs/samtools/$sample_name.log
+         cut -f1,3 | \ # cuts to output only readlabel and construct, e.g. : 0115c43a-e552-4415-8c32-cea083bc10b4	chr8
+         sort -k1 > library_preparation/bowtie/$sample_name/mapped_$sample_name.txt 2>> logs/samtools/$sample_name.log # sort by read label, (first col, above), and header is placed at bottom of the file.
+samtools view -f 4 library_preparation/bowtie/$sample_name/$sample_name.sam | \ # output only mappings with flag 4 set=read unmapped. See: http://www.htslib.org/doc/samtools-view.html flag description: https://broadinstitute.github.io/picard/explain-flags.html
+         cut -f1,10 | \ # print columns 1 and 10 == readID and sequence.
+         sort -k1> library_preparation/bowtie/$sample_name/unmapped_$sample_name.txt 2>> logs/samtools/$sample_name.log # sort by first col = readID
 
 #stats
 echo "Calculating stats"
