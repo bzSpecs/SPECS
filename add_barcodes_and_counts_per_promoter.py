96d7bbe3fd78238f89ed9138e4d0039963d70667
bzSpecs <shabib3>
2020-12-07 02:09:28 +0200

    formating

--- add_barcodes_and_counts_per_promoter.py
+++ add_barcodes_and_counts_per_promoter.py
@@ -14,42 +14,20 @@ results_by_promoter_df = pd.read_csv(results_by_promoter_csv_file)
 results_by_barcode_df = pd.read_csv(results_by_barcode_csv_file)
 paired_df = pd.read_csv(paired_mapping_path)
 
-barcodes_df_with_promoters = (
-    results_by_barcode_df.set_index("barcode")
-    .join(paired_df.set_index("barcode"))
-    .dropna()
-    .reset_index()
-)
+barcodes_df_with_promoters = results_by_barcode_df.set_index(
+    "barcode").join(paired_df.set_index("barcode")).dropna().reset_index()
 
-promoters_with_barcodes_count_df = (
-    results_by_promoter_df.set_index("promoter")
-    .join(
-        barcodes_df_with_promoters.set_index("promoter"),
-        lsuffix="_by_promoter",
-        rsuffix="_by_barcode",
-    )
-    .sort_values(by=["count_by_barcode"], ascending=False)
-    .reset_index()
-)
+promoters_with_barcodes_count_df = results_by_promoter_df.set_index("promoter").join(barcodes_df_with_promoters.set_index(
+    "promoter"), lsuffix="_by_promoter", rsuffix="_by_barcode", ).sort_values(by=["count_by_barcode"], ascending=False).reset_index()
 
-promoters_with_barcodes_count_df["barcode_and_count"] = (
-    promoters_with_barcodes_count_df["barcode"]
-    + "-"
-    + promoters_with_barcodes_count_df["count_by_barcode"].astype(str)
-)
-promoters_with_barcodes_count_df = (
-    promoters_with_barcodes_count_df.drop(columns=["count_by_barcode"])
-    .rename(columns={"count_by_promoter": "count"})
-    .reset_index()
-)
-promoters_with_barcodes_count_df = (
-    promoters_with_barcodes_count_df.groupby(["promoter", "count"])["barcode_and_count"]
-    .apply(", ".join)
-    .reset_index()
-    .rename(columns={"barcode_and_count": "barcodes_and_count"})
-    .sort_values(by=["count"], ascending=False)
-    .reset_index(drop=True)
-)
+promoters_with_barcodes_count_df["barcode_and_count"] = promoters_with_barcodes_count_df["barcode"] + \
+    "-" + promoters_with_barcodes_count_df["count_by_barcode"].astype(str)
+
+promoters_with_barcodes_count_df = promoters_with_barcodes_count_df.drop(
+    columns=["count_by_barcode"]).rename(columns={"count_by_promoter": "count"}).reset_index()
+
+promoters_with_barcodes_count_df = promoters_with_barcodes_count_df.groupby(["promoter", "count"])["barcode_and_count"].apply(
+    ", ".join).reset_index().rename(columns={"barcode_and_count": "barcodes_and_count"}).sort_values(by=["count"], ascending=False).reset_index(drop=True)
 
 # create the folder if not exists yet
 if not os.path.exists(os.path.dirname(output_csv)):
