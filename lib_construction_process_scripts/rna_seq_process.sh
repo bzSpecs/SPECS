f06a104d96eca010bdb59ede77e656c5d649c046
Blosberg <bosberg@dal.ca>
2020-12-23 16:13:20 +0100

    Set minimum 20 output

--- lib_construction_process_scripts/rna_seq_process.sh
+++ lib_construction_process_scripts/rna_seq_process.sh
@@ -4,6 +4,8 @@ start=$(date +"%s")
 fastq=$1 # reads file
 sample_name=$2 # proper name this sample will be called
 
+# cutadapt="/Users/barzaruk/.local/bin/cutadapt"
+
 mkdir -p RNA_seq_results
 mkdir -p RNA_seq_results/$sample_name
 mkdir -p logs/RNA_seq_results
@@ -12,8 +14,8 @@ mkdir -p logs/RNA_seq_results
 mkdir -p RNA_seq_results/$sample_name/cut_adapt_out
 mkdir -p logs/RNA_seq_results/cut_adapt_out
 echo "Clipping barcodes"
-cutadapt -g AACCAGCTCGGATCC \
-         -O 3 -n 2 -m 22 -M 25\
+${cutadapt} -g AACCAGCTCGGATCC \
+         -O 3 -n 2 -m 20 -M 25\
          -o RNA_seq_results/$sample_name/cut_adapt_out/clipped_$sample_name.fastq  \
          --untrimmed-output RNA_seq_results/$sample_name/cut_adapt_out/unclipped_$sample_name.fastq \
          $fastq \
