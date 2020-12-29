e256f03691c15dac1226f19f96479acec8b4a80d
Bar Zaruk <bzaruk@gmail.com>
2020-12-29 14:06:09 +0200

    renames file

--- filter_entries_with_reverse.py
+++ filter_entries_with_reverse.py
@@ -25,5 +25,3 @@ if not os.path.exists(os.path.dirname(output_file)):
     os.makedirs(os.path.dirname(output_file))
 
 df.to_csv(output_file, index=False)
-
-# python filter_results_greater_than.py results_2/sum_all_replicate/ESC/ESC_sum.csv 400 results_2/sum_all_replicate/ESC/ESC_sum_above_400.csv
