a18e2d0e90685806af8ec87ca988e077602b5ca2
Blosberg <bosberg@dal.ca>
2021-01-18 17:30:16 +0100

    Documentation + README

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # SPECS
+
+
+
+# Initial library construction command is run to associate promoters with barcodes:
+./all_steps.sh <promoters.fastq> <barcodes.fastq>  <sample_name>
+# Was run with values:
+# Library_Construction_srcData_in/lib_constuction_promoters.fastq
+# Library_Construction_srcData_in/lib_constuction_barcodes.fastq
+# LibConstruction_
+# output sent by default to library_preparation/
+
+
+# Now RNAseq
+
+#./rna_seq_process.sh /project/Brendan_dev/DIP/RNA-seq/ESC/ESC_2/ESC_bio_2_tech_2.fastq  ESC_bio2_tech2kk
--- lib_construction_process_scripts/rna_seq_process.sh
+++ lib_construction_process_scripts/rna_seq_process.sh
@@ -6,17 +6,17 @@ sample_name=$2 # proper name this sample will be called
 
 # cutadapt="/Users/barzaruk/.local/bin/cutadapt"
 
-mkdir -p RNA_seq_results
-mkdir -p RNA_seq_results/$sample_name
-mkdir -p logs/RNA_seq_results
+mkdir -p RNA_preprocess_clipBarcodes
+mkdir -p RNA_preprocess_clipBarcodes/${sample_name}
+mkdir -p RNA_preprocess_clipBarcodes/logs
 
 #clip barcodes:
-mkdir -p RNA_seq_results/$sample_name/cut_adapt_out
-mkdir -p logs/RNA_seq_results/cut_adapt_out
+mkdir -p RNA_preprocess_clipBarcodes/${sample_name}/cut_adapt_out
+mkdir -p RNA_preprocess_clipBarcodes/logs/cut_adapt_out
 echo "Clipping barcodes"
 ${cutadapt} -g AACCAGCTCGGATCC \
          -O 3 -n 2 -m 20 -M 25\
-         -o RNA_seq_results/$sample_name/cut_adapt_out/clipped_$sample_name.fastq  \
-         --untrimmed-output RNA_seq_results/$sample_name/cut_adapt_out/unclipped_$sample_name.fastq \
+         -o RNA_preprocess_clipBarcodes/${sample_name}/cut_adapt_out/clipped_${sample_name}.fastq  \
+         --untrimmed-output RNA_preprocess_clipBarcodes/${sample_name}/cut_adapt_out/unclipped_${sample_name}.fastq \
          $fastq \
-         1>>logs/RNA_seq_results/cut_adapt_out/R1_$sample_name.log 2>&1
+         1>> RNA_preprocess_clipBarcodes/logs/cut_adapt_out/R1_${sample_name}.log 2>&1
