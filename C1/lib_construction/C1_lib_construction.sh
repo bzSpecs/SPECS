52f01cc75c5753b1dab2e46066342a93da1cef44
Tzlil Yair <tzlil.yair@cs.huji.ac.il>
2021-10-20 13:25:19 +0300

    change source of programs

--- C1/lib_construction/C1_lib_construction.sh
+++ C1/lib_construction/C1_lib_construction.sh
@@ -12,7 +12,11 @@ output_root_dir=$9
 
 sample_folder=$output_root_dir/$experiment_name/$cell_line_name/$replicate_name/$sample_name
 
-source $define_local_exec_paths_file
+
+# to run on cluster:
+module load hurcs
+module load cutadapt/3.4
+#source $define_local_exec_paths_file
 
 mkdir -p $sample_folder/17_unique/cutadapt
 mkdir -p $sample_folder/17_unique/alignment
@@ -26,15 +30,19 @@ mkdir -p $sample_folder/barcodes/cutadapt
 # leftover: GGA
 
 # 17 unique
-$cutadapt -j 0 -g TGCGATCTAAGTAAGCTTG -a GTACTGTTGGTAAACCAGCTC -m 17 -M 17 -O 3 -n 2 -o $sample_folder/17_unique/cutadapt/clipped_17_unique_output.fastq --untrimmed-output $sample_folder/17_unique/cutadapt/unclipped_17_unique_output.fastq --too-short-output $sample_folder/17_unique/cutadapt/too_short_17_unique_output.fastq --too-long-output  $sample_folder/17_unique/cutadapt/too_long_17_unique_output.fastq $file
+cutadapt -j 0 -g TGCGATCTAAGTAAGCTTG -a GTACTGTTGGTAAACCAGCTC -m 17 -M 17 -O 3 -n 2 -o $sample_folder/17_unique/cutadapt/clipped_17_unique_output.fastq --untrimmed-output $sample_folder/17_unique/cutadapt/unclipped_17_unique_output.fastq --too-short-output $sample_folder/17_unique/cutadapt/too_short_17_unique_output.fastq --too-long-output  $sample_folder/17_unique/cutadapt/too_long_17_unique_output.fastq $file
 
-$bowtie2 --very-sensitive --norc --met-file $sample_folder/17_unique/alignment/met_17_unique.txt -p 4 -x $bowtie2_referece_folder -U $sample_folder/17_unique/cutadapt/clipped_17_unique_output.fastq -S $sample_folder/17_unique/alignment/new_17_unique.sam &> $sample_folder/17_unique/alignment/new_17_unique.log
+bowtie2 --very-sensitive --norc --met-file $sample_folder/17_unique/alignment/met_17_unique.txt -p 4 -x $bowtie2_referece_folder -U $sample_folder/17_unique/cutadapt/clipped_17_unique_output.fastq -S $sample_folder/17_unique/alignment/new_17_unique.sam &> $sample_folder/17_unique/alignment/new_17_unique.log
 
-$samtools view -F 4 $sample_folder/17_unique/alignment/new_17_unique.sam | cut -f1,3 | sort -k1 > $sample_folder/17_unique/alignment/new_17_unique.txt 2>> $sample_folder/17_unique/alignment/new_17_unique_samtools.log
+samtools view -F 4 $sample_folder/17_unique/alignment/new_17_unique.sam | cut -f1,3 | sort -k1 > $sample_folder/17_unique/alignment/new_17_unique.txt 2>> $sample_folder/17_unique/alignment/new_17_unique_samtools.log
 
 # barcodes           
-$cutadapt -j 0 -g GTACTGTTGGTAAACCAGCTC -O 3 -n 2 -o $sample_folder/barcodes/cutadapt/clipped_barcodes_output.fastq --untrimmed-output $sample_folder/barcodes/cutadapt/unclipped_barcodes_output.fastq --too-short-output $sample_folder/barcodes/cutadapt/too_short_clipped_barcodes_output.fastq --too-long-output $sample_folder/barcodes/too_long_clipped_barcodes_output.fastq $file
+cutadapt -j 0 -g GTACTGTTGGTAAACCAGCTC -O 3 -n 2 -o $sample_folder/barcodes/cutadapt/clipped_barcodes_output.fastq --untrimmed-output $sample_folder/barcodes/cutadapt/unclipped_barcodes_output.fastq --too-short-output $sample_folder/barcodes/cutadapt/too_short_clipped_barcodes_output.fastq --too-long-output $sample_folder/barcodes/too_long_clipped_barcodes_output.fastq $file
 
+python -m pip install pandas
+python -c "import pandas"
+python -m pip install ipdb
+python -c "import ipdb"
 
 python $pyscripts_folder/r1_to_tsv.py $sample_folder/barcodes/cutadapt/clipped_barcodes_output.fastq $sample_folder/barcodes/barcodes_mapping.tsv
 python $pyscripts_folder/pair_barcodes_to_unique_17.py $sample_folder/17_unique/alignment/new_17_unique.txt $sample_folder/barcodes/barcodes_mapping.tsv $sample_folder/paired.csv
